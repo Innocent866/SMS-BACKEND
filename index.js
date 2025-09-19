53094411baa22a8d83431e75b8195bb353d52684
Innocent866 <igoldima@gmail.com>
2025-09-20 00:22:53 +0100

    Update

---
+++ index.js
@@ -0,0 +1,22 @@
+// Server entering
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Simple route (First route)
+app.get('/', (req, res)=>{
+    res.send("Home Page");
+    console.log("Home Page");
+})
+
+// Start Server
+app.listen(PORT, ()=>{
+    console.log(`Server listening to port: http://localhost:${PORT}`);
+})
